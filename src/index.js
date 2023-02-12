6f9c8c2b8cc210a04d56c50453c3f1f2b6c0a29e
AlexBoronin <acivinel@mail.ru>
2023-02-12 14:32:02 +0300

    feat: add variables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function toReadable (number) {
+    let units = ['zero', ' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine', ' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen'];
+    let dozens = ['', '', ' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty', ' ninety'];
+    let hundreds = ' hundred';
+    let out = '';
+    let numToString = number.toString();
 
 }
