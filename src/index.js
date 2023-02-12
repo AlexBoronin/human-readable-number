be28b573ad0c48e1d11f079d604dd6fadc987545
AlexBoronin <acivinel@mail.ru>
2023-02-12 18:29:10 +0300

    refactor: reload task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-// module.exports =
-function toReadable (number) {
+module.exports =function toReadable (number) {
     let units = ['', ' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine', ' ten', ' eleven',
                 ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen'];
     let dozens = ['', '', ' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty', ' ninety'];
@@ -7,8 +6,11 @@ function toReadable (number) {
 
     let numToString = number.toString();
 
+    if (number === 0) {
+        return 'zero'
+    }
     if(number < 20){
-        return units[number]
+        return (units[number]).trim()
     }else if(numToString.length === 3){
         out += units[parseInt(numToString[0])] + ' hundred';
         if(parseInt(numToString[1]) === 1){
@@ -20,5 +22,4 @@ function toReadable (number) {
     }
     return (dozens[parseInt(numToString[0])] + units[parseInt(numToString[1])]).trim()
 }
-console.log(toReadable('121'));
 
